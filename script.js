898c9674c1f5688ae6156d798d4376258311375a
Lincoln Mota <116844681+LincolnMota07@users.noreply.github.com>
2024-01-19 15:47:31 -0300

    atualização Responsividade  UX

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 const swiper = new Swiper('.swiper', {
     
-    slidesPerView: 1,
-    spaceBetween: 10,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-     
-    },
-    loop: true,
+  slidesPerView: 1,
+  spaceBetween: 10,
+  breakpoints: {
+    1728: {
+      slidesPerView: 3,
+      spaceBetween: 100
+    }
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+   
+  },
+  loop: true,
 
-  });
+});
+
+
+
+
+
 
 
